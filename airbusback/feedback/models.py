25d6ceccc93d2cd6a55009592f26aab3c58bd87a
Lokesh2703 <lokesh27dinu@gmail.com>
2021-05-19 21:14:48 +0530

    add user to feedback

--- airbusback/feedback/models.py
+++ airbusback/feedback/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
 class Feedback(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
     title = models.CharField(max_length=250,null=False)
     description = models.TextField(null=False)
     created_time = models.DateTimeField(null=False,auto_now_add=True)

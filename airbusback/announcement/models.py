acc72e540ae9e52df4fd50a6fdce0adb12d2b396
shoaib <temp786786786@gmail.com>
2021-05-20 13:54:45 +0530

    webIntegration

---
+++ airbusback/announcement/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Announcement(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    created_time = models.TimeField(auto_now_add=True)
+    last_update_time = models.TimeField(auto_now=True)

6a0c2e384ccf63c0de2662462378cf252e85c2a3
Lokesh2703 <lokesh27dinu@gmail.com>
2021-05-20 23:07:48 +0530

    Add BugReport POST API

--- airbusback/feedback/urls.py
+++ airbusback/feedback/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path,include
 from .views import get_bugReport, add_feedback, get_topics,add_bugReport
-
 urlpatterns = [
     path('add/',add_feedback,name='add-feedback'),
     path('bug/get/',get_bugReport,name='get-bugreport'),
-    path('bug/add/',add_bugReport,name="add-bugreport"),
+    path('bug/add/',add_bugReport.as_view(),name="add-bugreport"),
     path('bug/topics/get/',get_topics,name='get-bugTopics'),
     # path('bug/topics/reports/get/',get_bugReportTopicwise,name='get-bugReportTopicwise'),
 ]
--- airbusback/feedback/views.py
+++ airbusback/feedback/views.py
@@ -1,9 +1,10 @@
+from rest_framework.views import APIView
 from .serializers import BugReport_Serializer, BugTopics_Serializer, Feedback_Serializer, BugReportAdd_Serializer
 from django.contrib.auth.models import AnonymousUser, User
 from django.shortcuts import render
 from rest_framework import serializers
 from django.http import HttpResponse, JsonResponse
-from rest_framework.parsers import JSONParser 
+from rest_framework.parsers import JSONParser, MultiPartParser, FormParser
 # Create your views here.
 from .models import Feedback, bugReport,bugTopics
 
@@ -39,25 +40,28 @@ def get_bugReport(request):
         serializer = BugReport_Serializer(bugReports,many=True)
         return JsonResponse(serializer.data,safe=False)
 
-@api_view(['POST'])
-def add_bugReport(request):
-
-    if request.method == 'POST':
+# @api_view(['POST'])
+class add_bugReport(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+    def post(self,request):
+        
         # print(request.data)
-        data = JSONParser().parse(request)
-        useremailtmp = User.objects.filter(email=data['user'])
-        topicnametmp = bugTopics.objects.filter(topicname=data['topic'])
-        if len(useremailtmp)>0:
-            data['user'] = useremailtmp[0].pk
-        if len(topicnametmp)>0:
-            data['topic'] = topicnametmp[0].pk
-
-        serializer = BugReportAdd_Serializer(data=data)
+        # data = JSONParser().parse(request)
+        # data = MultiPartParser()
+        # useremailtmp = User.objects.filter(email=request.get('user'))
+        # topicnametmp = bugTopics.objects.filter(topicname=request.get('topic'))
+        # if len(useremailtmp)>0:
+        #     request.set['user'] = useremailtmp[0].pk
+        # if len(topicnametmp)>0:
+        #     request.data['topic'] = topicnametmp[0].pk
+        # print(request.POST.set('user'))
+        print(request.data)
+        serializer = BugReportAdd_Serializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data,safe=False)
+            return Response(serializer.data)
         
-        return JsonResponse(serializer.errors,status=400)
+        return Response(serializer.errors,status=400)
 
 @api_view(['GET'])
 def get_topics(request):
